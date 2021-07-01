0a5fe2b9e698e96a5ab48c03aa43eb2e7aaf6ab7
Vlad <twister.09@mail.ru>
2021-07-01 19:31:01 +0300

    init commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias fuga
+        repudiandae aperiam quam, magnam maiores perspiciatis corrupti debitis
+        unde cupiditate qui praesentium ducimus, nobis eligendi deleniti. Neque
+        quos deleniti odio, nisi odit veniam dolorum maiores? Aspernatur non
+        sint nesciunt deserunt!
+      </p>
+    </section>
+  )
+}
+
+export default About
